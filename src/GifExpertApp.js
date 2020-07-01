833ed64a33fe37fbbb810b74ff7b32c6715e4d16
Nelson Cadenas <nca1478@yahoo.com>
2020-07-01 13:33:14 -0400

    71. Componente AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,26 +1,23 @@
 import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory'
 
 export const GifExpertApp = () => {
-  // const categories = ['One Punch', 'Samurai X', 'Dragon Ball']
   const [categories, setCategories] = useState([
     'One Punch',
     'Samurai X',
     'Dragon Ball'
   ])
 
-  const handleAdd = () => {
-    // Solución #1
-    // setCategories(['HunterXhunter', ...categories])
-
-    // Solución #2
-    setCategories((category) => ['HunterXhunter', ...categories])
-  }
+  // const handleAdd = () => {
+  //   setCategories(['HunterXhunter', ...categories])
+  // }
 
   return (
     <>
       <h2>GitExpertApp</h2>
+      <AddCategory />
       <hr />
-      <button onClick={handleAdd}>Agregar</button>
+      {/* <button onClick={handleAdd}>Agregar</button> */}
       <ol>
         {categories.map((category) => {
           return <li key={category}>{category}</li>
