527efe7c737a52e7a08001a9bb1b9dd643f9fae0
Nelson Cadenas <nca1478@yahoo.com>
2020-07-02 19:50:06 -0400

    Docs agregado

---
+++ docs/precache-manifest.143550a477dd01eb68123c645f024c52.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d564fe9009bb5f0f19ae0c1425fca77f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "752401d419e2afcebe40",
+    "url": "/static/css/2.e2dd9b0a.chunk.css"
+  },
+  {
+    "revision": "4fb7c4922921a7275ff4",
+    "url": "/static/css/main.4ac83be3.chunk.css"
+  },
+  {
+    "revision": "752401d419e2afcebe40",
+    "url": "/static/js/2.2f540f17.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2f540f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fb7c4922921a7275ff4",
+    "url": "/static/js/main.0814c1c6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
